873d58cbc01727711510a7e9d2bc40a505bd580d
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-18 13:23:23 +0300

    Done with making some variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './styles/reset.css'
+import './styles/variables.css'
 import './styles/style.css'
 import './styles/responsive.css'
 
